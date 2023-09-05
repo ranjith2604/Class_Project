dda5b867e44e3e58719d9b9b0868b0078a1c81be
ranjith2604 <85279774+ranjith2604@users.noreply.github.com>
2023-09-05 12:58:32 -0500

    Add files via upload

---
+++ Class_module.java
@@ -0,0 +1,11 @@
+package Class_package;
+
+public class Class_module {
+	
+	public class HelloWorld {
+	    public static void main(String[] args) {
+	        System.out.println("Hello, World!");
+	    }
+	}
+
+}
